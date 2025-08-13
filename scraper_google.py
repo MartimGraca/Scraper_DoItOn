874111e9fb12042edc12c23b0ede61c2d883ef03
RucaNasDrogas <martim.graca@my.istec.pt>
2025-08-13 16:14:49 +0100

    tirar headless

--- scraper_google.py
+++ scraper_google.py
@@ -237,7 +237,7 @@ def proxima_pagina(driver):
 
 def executar_scraper_google(keyword, filtro_tempo):
     options = uc.ChromeOptions()
-    options.add_argument('--headless=new')
+   # options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
